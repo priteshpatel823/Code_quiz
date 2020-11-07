ecd447d7da771e804a278353a6137ab981eecc3e
pritesh <priteshpatel823@gmail.com>
2020-11-07 15:31:09 -0600

    complete html & css for quiz

---
+++ question.js
@@ -0,0 +1,27 @@
+var questionschoice =[
+    {
+        title: "What is the capital of Australia?",
+        choices: ["<java>", "<body>", "<script>", "<img>"],
+        answer: "<script>"
+    },
+    {
+        title: "What is the longest river in the world?",
+        choices: ["<java>", "<body>", "<script>", "<img>"],
+        answer: "<script>"
+    },
+    {
+        title: "What is the second largest continent?",
+        choices: ["<java>", "<body>", "<script>", "<img>"],
+        answer: "<script>"
+    },
+    {
+        title: "Name one of the national animals of the USA?",
+        choices: ["<java>", "<body>", "<script>", "<img>"],
+        answer: "<script>"
+    },
+    {
+        title: "Which river flows through Paris?",
+        choices: ["<java>", "<body>", "<script>", "<img>"],
+        answer: "<script>"
+    },
+];
